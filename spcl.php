446e204d25fe1ee1597950d42848371cc16b857c
sergej.mueller <sergej.mueller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-10 05:47:33 +0000

    Add copyright & license information
    git-svn-id: https://plugins.svn.wordpress.org/spcl/trunk@929377 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- spcl.php
+++ spcl.php
@@ -5,7 +5,26 @@ Description: Bei der Speicherung der Artikel prüft das Plugin die im Text vorha
 Author: Sergej M&uuml;ller
 Author URI: http://wpcoder.de
 Plugin URI: https://plus.google.com/110569673423509816572/posts/hDtKSyEozeR
+License: GPLv2 or later
 Version: 0.6.0
+*/
+
+/*
+Copyright (C)  2011-2014 Sergej Müller
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 
